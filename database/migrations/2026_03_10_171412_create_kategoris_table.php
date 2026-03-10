c209d82a664ad800deaf5475badd063302cf50d0
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-03-11 00:26:34 +0700

    perbaikan code create kategoro

--- database/migrations/2026_03_10_171412_create_kategoris_table.php
+++ database/migrations/2026_03_10_171412_create_kategoris_table.php
@@ -1,9 +1,23 @@
-public function up(): void
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
 {
-    Schema::create('kategoris', function (Blueprint $table) {
-        $table->id();
-        $table->foreignId('product_id')->constrained()->onDelete('cascade');
-        $table->string('name');
-        $table->timestamps();
-    });
-}
+    public function up(): void
+    {
+        Schema::create('kategoris', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('kategoris');
+    }
+};
