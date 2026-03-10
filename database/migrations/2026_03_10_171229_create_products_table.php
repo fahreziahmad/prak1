60adff97debce57edf635531081c51fe913f1ee5
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-03-11 00:25:48 +0700

    perbaikan code product

--- database/migrations/2026_03_10_171229_create_products_table.php
+++ database/migrations/2026_03_10_171229_create_products_table.php
@@ -1,11 +1,25 @@
-public function up(): void
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
 {
-    Schema::create('products', function (Blueprint $table) {
-        $table->id();
-        $table->foreignId('user_id')->constrained()->onDelete('cascade');
-        $table->string('name');
-        $table->integer('qty');
-        $table->decimal('price', 10, 2);
-        $table->timestamps();
-    });
-}
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('name');
+            $table->integer('qty');
+            $table->decimal('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
