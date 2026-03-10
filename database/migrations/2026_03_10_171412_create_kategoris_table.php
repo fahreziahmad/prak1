31e07194153d7e5ef469a144a35bac0634321462
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-03-11 00:19:54 +0700

    add code katagori table

--- database/migrations/2026_03_10_171412_create_kategoris_table.php
+++ database/migrations/2026_03_10_171412_create_kategoris_table.php
@@ -1,27 +1,9 @@
-<?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
+public function up(): void
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('kategoris', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('kategoris');
-    }
-};
+    Schema::create('kategoris', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('product_id')->constrained()->onDelete('cascade');
+        $table->string('name');
+        $table->timestamps();
+    });
+}
